3fa47f810490f9cf92c4ac9d0177d2715c1c7966
inesserra <ines.isfserra@gmail.com>
2024-05-26 18:19:28 +0100

    added homepage link + final touches

--- js/index.js
+++ js/index.js
@@ -1,29 +1,20 @@
 function updateTime() {
-  //los angeles
-  let losAngelesElement = document.querySelector("#los-angeles");
-  if (losAngelesElement) {
-    let losAngelesDateElement = losAngelesElement.querySelector(".date");
-    let losAngelesTimeElement = losAngelesElement.querySelector(".time");
-    let losAngelesTime = moment().tz("America/Los_Angeles");
+  const cities = [
+    { id: "lisbon", timezone: "Europe/Lisbon" },
+    { id: "los-angeles", timezone: "America/Los_Angeles" },
+    { id: "sydney", timezone: "Australia/Sydney" },
+  ];
+  cities.forEach((city) => {
+    const cityElement = document.querySelector(`#${city.id}`);
+    if (cityElement) {
+      const dateElement = cityElement.querySelector(".date");
+      const timeElement = cityElement.querySelector(".time");
+      const time = moment().tz(city.timezone);
 
-    losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
-    losAngelesTimeElement.innerHTML = losAngelesTime.format(
-      "h:mm:ss [<small>]A[</small>]"
-    );
-  }
-
-  //sydney
-  let sydneyElement = document.querySelector("#sydney");
-  if (sydneyElement) {
-    let sydneyDateElement = sydneyElement.querySelector(".date");
-    let sydneyTimeElement = sydneyElement.querySelector(".time");
-    let sydneyTime = moment().tz("Australia/Sydney");
-
-    sydneyDateElement.innerHTML = sydneyTime.format("MMMM Do YYYY");
-    sydneyTimeElement.innerHTML = sydneyTime.format(
-      "h:mm:ss [<small>]A[</small>]"
-    );
-  }
+      dateElement.innerHTML = time.format("MMMM Do YYYY");
+      timeElement.innerHTML = time.format("h:mm:ss [<small>]A[</small>]");
+    }
+  });
 }
 
 function updateCity(event) {
@@ -43,7 +34,11 @@ function updateCity(event) {
         <div class="time">${cityTime.format(
           "h:mm:ss [<small>]A[</small>]"
         )}</div>
-    </div>`;
+      
+        
+    </div>
+    <div class="homepage">
+        <a href="index.html" >Go to Homepage</a></div>`;
 }
 
 updateTime();
